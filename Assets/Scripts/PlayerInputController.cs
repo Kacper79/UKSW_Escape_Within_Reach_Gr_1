c6a6e30bcf23747408f10f3ad0779ccff81e6ccd
HubsonB <Hubert@student.uksw.edu.pl>
2024-10-31 20:52:22 +0100

    Player movement
    Poruszanie się, wspinanie i new input system

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    private PlayerInput player_input;
+
+    private void Awake()
+    {
+        player_input = new();
+
+        GetComponentInChildren<PlayerInteractionController>().SetPlayerInput(player_input);
+        GetComponentInChildren<PlayerMovementController>().SetPlayerInput(player_input);
+        GetComponentInChildren<PlayerAttackingController>().SetPlayerInput(player_input);
+    }
+}
